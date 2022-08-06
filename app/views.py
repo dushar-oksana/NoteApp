9d74d28530f42a7c481e5ee72223205781e7753f
Oksana Dushar <dushar0937@gmail.com>
2022-08-06 21:48:07 +0200

    Added Create Route

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, redirect, Blueprint
 
 main_blueprint = Blueprint('main', __name__)
 
@@ -6,3 +6,9 @@ main_blueprint = Blueprint('main', __name__)
 @main_blueprint.route('/')
 def index():
     return render_template('index.html')
+
+@main_blueprint.route('/create')
+def update():
+    # add code here
+    return redirect('/')
+
